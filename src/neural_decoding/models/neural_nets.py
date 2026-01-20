c616adaba50e0ad1893f49aa3ea2b4753f15cb27
Kidus <kidusabebe1921@gmail.com>
2026-01-20 09:20:16 +0200

    removed unnecessary models

--- src/neural_decoding/models/neural_nets.py
+++ src/neural_decoding/models/neural_nets.py
@@ -16,28 +16,6 @@ class DenseNNDecoder(BaseDecoder):
         pass
 
 
-class SimpleRNNDecoder(BaseDecoder):
-    def __init__(self, units: int = 400, dropout_rate: float = 0.25, num_epochs: int = 10, batch_size: int = 128, verbose: int = 1):
-        pass
-
-    def fit(self, X: np.ndarray, y: np.ndarray) -> "SimpleRNNDecoder":
-        pass
-
-    def predict(self, X: np.ndarray) -> np.ndarray:
-        pass
-
-
-class GRUDecoder(BaseDecoder):
-    def __init__(self, units: int = 400, dropout_rate: float = 0.25, num_epochs: int = 10, batch_size: int = 128, verbose: int = 1):
-        pass
-
-    def fit(self, X: np.ndarray, y: np.ndarray) -> "GRUDecoder":
-        pass
-
-    def predict(self, X: np.ndarray) -> np.ndarray:
-        pass
-
-
 class LSTMDecoder(BaseDecoder):
     def __init__(self, units: int = 400, dropout_rate: float = 0.25, num_epochs: int = 10, batch_size: int = 128, verbose: int = 1):
         pass
